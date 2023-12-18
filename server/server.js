098fcff6dc0c592da1b1f7988be39170602a6815
Anaïs <aekyon.08@gmail.com>
2023-12-18 09:41:43 +0100

    Import mariadb

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+//IMPORT MARIADB
 import mariadb from 'mariadb'
 import * as dotenv from 'dotenv'
 dotenv.config()
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    connectionLimit: 5
+})
