a969c29144652b975a0d535049527b36a597d3c6
Anaïs <aekyon.08@gmail.com>
2023-12-18 11:31:32 +0100

    Connect table to server on root level

--- server/server.js
+++ server/server.js
@@ -1,33 +1,30 @@
 // //IMPORT MARIADB
-// import mariadb from 'mariadb'
-// import * as dotenv from 'dotenv'
-// dotenv.config()
+import mariadb from 'mariadb'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-// const pool = mariadb.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     connectionLimit: 5
-// })
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    connectionLimit: 5
+})
 
-// //USE POOL AND QUERIES
-// (async () => {
-//     let connection;
-//     try {
-//         connection = await pool.getConnection();
-//         const data = await connection.query(`SELECT * FROM brilliant_minds.ideas`);
-//         console.log(data)
-//     } catch(err) {
-//         throw err;
-//     } finally {
-//         if (connection) connection.end();
-//     }
-// })()
+const connectTable = async () => {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const data = await connection.query(`SELECT * FROM brilliant_minds.ideas`);
+        console.log(data)
+    } catch(err) {
+        throw err;
+    } finally {
+        if (connection) connection.end();
+    }
+}
 
-//IMPORT EXPRESS
+//IMPORT EXPRESS AND USE APP
 import express from "express";
-
-//APP
 const app = express();
 // app.use(express.json());
 /*url encoded*/
@@ -38,5 +35,7 @@ app.listen(8000, () => {
 
 //SERVER ROUTE
 app.get("/", (req, res) => {
-    res.send({status:200, msg:"This is the root"})
+    // res.send({status:200, msg:"This is the root"})
+    connectTable();
 })
+
