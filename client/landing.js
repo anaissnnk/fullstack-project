858dc34592d710bb11855fa75db25ce119a3f55a
Anaïs <aekyon.08@gmail.com>
2023-12-19 14:18:28 +0100

    Change file names to landing

---
+++ client/landing.js
@@ -0,0 +1,16 @@
+//FETCH AND INJECT DATA 
+(async () => {
+    const response = await fetch('http://localhost:8000/');
+    const toDoList = await response.json();
+
+    const toDoSection = document.querySelector('.to-do-list');
+
+    toDoList.forEach(todo => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${todo.id}: ${todo.title}: ${todo.description}`;
+        toDoSection.appendChild(listItem);
+    });
+})();
+
+
+
