7f7c8858fd05b0a7539c5360722d1d1b564ba80c
Anaïs <aekyon.08@gmail.com>
2023-12-20 17:31:43 +0100

    Add redirection between pages

--- client/landing.js
+++ client/landing.js
@@ -19,15 +19,41 @@
     });
 })();
 
-// //DELETE DATA
-// const deleteButton = document.getElementsByClassName("delete-button");
-// const buttonsArray = Array.from(deleteButton);
+//DELETE DATA
+const deleteButton = document.getElementsByClassName("delete-button");
+const buttonsArray = Array.from(deleteButton);
 
-// buttonsArray.forEach(button => {
-//     button.addEventListener("click", async() => {
-//         await pool.getConnection();
-//         const removeIdea = await connect.query(
-//             "DELETE FROM ideas WHERE id=?"
-//         )
-//     })
-// })
+buttonsArray.forEach(button => {
+    button.addEventListener("click", async() => {
+        await pool.getConnection();
+        const removeIdea = await connect.query(
+            "DELETE FROM ideas WHERE id=?"
+        )
+    })
+})
+
+// // Assuming you have a collection of delete buttons
+// const deleteButtons = document.querySelectorAll('.delete-button');
+
+// deleteButtons.forEach(button => {
+//     button.addEventListener('click', async () => {
+//         try {
+//             const id = button.id;
+//             const response = await fetch(`http://localhost:8000/delete/${id}`, {
+//                 method: 'DELETE'
+//             });
+            
+//             if (response.ok) {
+//                 console.log('Item deleted successfully!');
+//                 const removeIdea = await connect.query(
+//                     "DELETE FROM ideas WHRE id=?"
+//                 )
+//             } else {
+//                 console.error('Failed to delete item.');
+//             }
+//         } catch (error) {
+//             console.error('Error:', error);
+//         }
+//     });
+// });
+
